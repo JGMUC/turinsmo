93f1d617069eb3079de6c32af0e17aeae6bd753b
Javier Garay <javier.garay@alfagl.com>
2023-05-30 12:01:17 -0500

    Se agregan posibilidad de desbloqueo de usuarios

---
+++ Backend/ingweb/src/main/java/com/turismo/ingweb/model/Municipio.java
@@ -0,0 +1,14 @@
+package com.turismo.ingweb.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@Entity
+public class Municipio {
+    
+}
